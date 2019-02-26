1d2d0658221c948daa6969099e0afae175cd3370
pfg <omegariisthebest@gmail.com>
2019-02-26 12:26:32 -0800

    Add autocomplete

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,29 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as actions from './actions.json';
+import * as actions from './actionCompletionInfo.json';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	let completionItems = Object.keys(actions).map(a=>{
+		let allActions = actions as {
+			[key: string]: {
+				docs: string,
+				args: {
+					argName?: string,
+					argType: string,
+					argAutocompletePlaceholder?: string
+				}[],
+				autocomplete?: string
+			}
+		};
+		let thisAction = allActions[a];
 		let item = new vscode.CompletionItem(a);
-		item.documentation = new vscode.MarkdownString((actions as {[key: string]: string})[a]);
+		item.documentation = new vscode.MarkdownString(thisAction.docs);
+		//item.detail = //TODO arguments
+		item.insertText = new vscode.SnippetString(thisAction.autocomplete);
 		return item;
 	});
 
